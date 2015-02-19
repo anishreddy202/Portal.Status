bb8d23176ce827f35037618313daa68193a75f97
Daniel Scholl <Daniel.Scholl@verizon.com>
2015-02-19 11:29:23 -0600

    Added Validation Schema checks

--- src/server/components/statusService/lib/validator.js
+++ src/server/components/statusService/lib/validator.js
@@ -1,49 +1,80 @@
 'use strict';
 var Joi = require('joi');
+var Verror = require("verror");
 var log = require('debug')('status:validator');
-
-// SCHEMA DEFINITION //
-
-
-var systemSchema = Joi.object().keys({
-
-});
-
-var servicesSchema = Joi.object().keys({
-
-});
-
-var servicesListSchema = Joi.array().includes(servicesSchema);
-var systemListSchema = Joi.array().includes(systemSchema);
-
-var categorySchema = Joi.object().keys({
-  code: Joi.string().valid(["CDN", "TRT", "RTE", "UPL"]).required(),
-  name: Joi.string().required(),
-  systems: systemListSchema,
-  services: servicesListSchema
-});
-
-var inputSchema = Joi.array().includes(categorySchema);
-
-
 
 var Validator = function(){
   var self = this;
+
+  self.validate = validateSchema;
+
+
+  ////////////   SCHEMA DEFINITION   ////////////
+
+  var categoryCodes = ["CDN", "TRT", "RTE", "UPL"];
+  var statusCodes = ["OK", "ERR", "MNT", "DGR"];
+  var systemCodes = ["MCC", "TCC", "LLS", "LIS", "ODS", "UCC"];
+  var serviceCodes = ["HLG", "HSM", "ADN", "ING", "DLY", "FTP", "STG", "PRG", "SRT", "ADA", "SZT", "API"];
+  var locationCodes = ["DCA", "IAD", "ATL", "FTY", "MDW", "ORD", "DFW", "FTW", "BUR", "CPM", "LAX", "OXR",
+                       "FLL", "MIA", "EWR", "LGA", "BNG", "RHV", "SJC", "CGH", "PAE", "SEA", "AMS", "RTM",
+                       "CPH", "FCN", "FRA", "FRF", "HEL", "LCY", "LHR", "MAD", "TOJ", "MXP", "CDG", "ORY",
+                       "ARN", "STO", "VIE", "WAW", "BTH", "HHP", "CGK", "KHH", "MEL", "KIX", "SIN", "SYD",
+                       "HND", "NRT", "MCC", "TCC", "UCC", "LIS", "LLS", "ODS"];
+
+  var locationSchema = Joi.array().includes(
+    Joi.object().keys({
+      code: Joi.string().valid(locationCodes).required(),
+      name: Joi.string().required(),
+      region: Joi.string().required()
+    })).required();
+
+  var serviceSchema = Joi.array().includes(
+    Joi.object().keys({
+      code: Joi.string().valid(serviceCodes).required(),
+      name: Joi.string().required(),
+      locations: locationSchema
+    })).required();
+
+  var systemSchema = Joi.array().includes(
+    Joi.object().keys({
+      code: Joi.string().valid(systemCodes).required(),
+      name: Joi.string().required(),
+      status: Joi.string().valid(statusCodes).required()
+    }))
+    .when('code', {is: 'RTE', then: Joi.optional(), otherwise: Joi.required()});
+
+  var categorySchema = Joi.object().keys({
+    code: Joi.string().valid(categoryCodes).required(),
+    name: Joi.string().required(),
+    systems: systemSchema,
+    services: serviceSchema
+  });
+
+  var schema = Joi.array().includes(categorySchema);
+
+  ////////////  END SCHEMA DEFINITION   ////////////
+
+
   var options = {
     stripUnknown: true,
     presence: "optional"
   };
 
-  self.validate = function(data, done){
-    Joi.validate(data, inputSchema, options, function(err, value){
+  function validateSchema(data, done){
+
+
+    Joi.validate(data, schema, options, function(err, value){
+      var error = null;
+
       if (err) {
- //       log('FAILED: ' + value + ' - ' + JSON.stringify(err, null, 2));
+        error = new Verror(err, 'Schema Validator Error');
+        log('FAILED: ' + value + ' - ' + JSON.stringify(err, null, 2));
       } else {
         log('OK: ' + JSON.stringify(value));
       }
-      done(err, value);
+      done(error, value);
     });
-  };
+  }
 
   return self;
 };
