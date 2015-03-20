09edce30d6a5c1432ec2ea0d1bff33e61889d1d7
v045101 <anishreddy202@gmail.com>
2015-03-20 15:26:49 -0500

    auth test cases added

--- src/server/components/auth/test/log.spec.js
+++ src/server/components/auth/test/log.spec.js
@@ -1,25 +1,25 @@
-//var should = require("should");
-//var Log = require("../../log");
-//
-//describe("Log", function() {
-//    describe("Defaults", function() {
-//        var log = {};
-//
-//        before(function() {
-//            log = new Log({subject: "Log Subject", entry: "Log Message", userId: 1});
-//        });
-//
-//        it("has a subject", function(){
-//            log.subject.should.equal("Log Subject");
-//        });
-//        it("has an entry", function(){
-//            log.entry.should.equal("Log Message");
-//        });
-//        it("has a userId", function(){
-//            log.userId.should.equal(1);
-//        });
-//        //it("has a created Date", function(){
-//        //    log.createdAt.should.be.defined;
-//        //});
-//    });
-//});
+var should = require("should");
+var Log = require("../../log");
+
+describe("Log", function() {
+    describe("Defaults", function() {
+        var log = {};
+
+        before(function() {
+            log = new Log({subject: "Log Subject", entry: "Log Message", userId: 1});
+        });
+
+        it("has a subject", function(){
+            log.subject.should.equal("Log Subject");
+        });
+        it("has an entry", function(){
+            log.entry.should.equal("Log Message");
+        });
+        it("has a userId", function(){
+            log.userId.should.equal(1);
+        });
+        //it("has a created Date", function(){
+        //    log.createdAt.should.be.defined;
+        //});
+    });
+});
